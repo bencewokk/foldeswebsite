4bf0676a24c29dd67754f4a0ff0cae31ad723dcc
Bencewok <bencewokk@gmail.com>
2024-11-26 23:41:28 +0100

    0.1

--- main.go
+++ main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*html")),
+	}
+}
+
+type Count struct {
+	Count int
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Renderer = newTemplate()
+
+	// Render index page
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", nil)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,25 @@
 ### Földes Ferenc Higschool website remade with golang-htmx stack
 ![bellcure](bellcurve2.png)
+
+## needed
+
+go install github.com/air-verse/air@latest
+go install golang.org/x/tools/gopls@latest
+
+
+## notes
+-news
+-tabs
+    -about
+        -achivements
+        -langugecert
+        -history
+    -for parents
+        -kreta
+        -korszeru iskola foundation
+        -consultation hours
+    -for students
+        -timetable
+        -school rules
+        -
+-calendar
